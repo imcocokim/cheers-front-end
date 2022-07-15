3474f30af92cf6134bf2b5a34f4d5000586d320c
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-15 11:28:08 -0400

    nav bar functionality

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,20 @@
 import { Link } from 'react-router-dom'
+import styles from './Nav.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
+        <nav className={styles.navbar}>
+          <ul className={styles.navitems}>
+            <li className={styles.navul}>Welcome, {user.name}</li>
+            <li className={styles.navul}><Link to="/add-song">Add Tune</Link></li>
+            <li className={styles.navul}><Link to="/boozy-tunes">Boozy Tunes</Link></li>
+            <li className={styles.navul}><Link to="/my-tastes">My Tastes</Link></li>
+            <li className={styles.navul}><Link to="/profiles">Profiles</Link></li>
+            <li className={styles.navul}><Link to="/changePassword">Change Password</Link></li>
+            <li className={styles.navul}><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+
           </ul>
         </nav>
       :
@@ -17,6 +22,11 @@ const NavBar = ({ user, handleLogout }) => {
           <ul>
             <li><Link to="/login">Log In</Link></li>
             <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/signup">Add Tune</Link></li>
+            <li><Link to="/signup">Boozy Tunes</Link></li>
+            <li><Link to="/signup">My Taste</Link></li>
+
+
           </ul>
         </nav>
       }
