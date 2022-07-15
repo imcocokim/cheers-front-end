7b180b3bac10c2d5b385a5ad2fad060d487eabdc
dlobera <delobera@gmail.com>
2022-07-15 16:13:53 -0500

    add drop down

--- src/pages/MyTastes/MyTastes.jsx
+++ src/pages/MyTastes/MyTastes.jsx
@@ -1,15 +1,17 @@
-import drinks from "../../../data/drink-data";
+
 
 const MyTastes = (props) => {
   const tastes = [...new Set(props.drinks.map(
     (drink) => drink.taste
   ))]
   return ( 
-  <>
-    <h1>My Tastes</h1>
-    <p>Dropdown Here</p>
-    <p>Tunes paired with Tastes cards Here</p>
-  </> 
+    <select onChange={(e) => props.setDrinkCategory(e.target.value)}>
+    {tastes.map((taste,idx) => (
+      <option key={idx} value={taste}>
+        {taste}
+      </option>
+    ))}
+  </select>
   );
 }
  
