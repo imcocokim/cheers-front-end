19318acfec5d90124761c769bc3c05b002af0629
dlobera <delobera@gmail.com>
2022-07-15 13:54:39 -0500

    stup up tastes const

--- src/pages/MyTastes/MyTastes.jsx
+++ src/pages/MyTastes/MyTastes.jsx
@@ -1,7 +1,12 @@
-const MyTastes = () => {
+const MyTastes = (props) => {
+  const tastes = [...new Set(props.drinks.map(
+    (drink) => drink.taste
+  ))]
   return ( 
   <>
     <h1>My Tastes</h1>
+    <p>Dropdown Here</p>
+    <p>Tunes paired with Tastes cards Here</p>
   </> 
   );
 }
