1cc1413afaede6e5d99b3ae96fbddf4f19c08145
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-15 15:05:28 -0400

    work on landing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
 import * as authService from './services/authService'
-import AddSong from './pages/AddSong/AddSong'
-import MyTastes from './pages/MyTastes/MyTastes'
+// import AddSong from './pages/AddSong/AddSong'
+// import MyTastes from './pages/MyTastes/MyTastes'
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
@@ -41,14 +41,14 @@ const App = () => {
           path="/profiles"
           element={user ? <Profiles /> : <Navigate to="/login" />}
         />
-        <Route
+        {/* <Route
           path="/add-song"
           element={<AddSong />}
         />
         <Route
           path="/my-tastes"
           element={<MyTastes />}
-        />
+        /> */}
         <Route
           path="/changePassword"
           element={
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,17 @@
-import styles from './Landing.module.css'
-
-const Landing = ({ user }) => {
+// import { Link } from 'react-router-dom'
+// import NavBar from '../../components/NavBar/NavBar'
+ 
+const Landing = ({ user, handleLogout }) => {
   return (
-    <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+    <main>
+
+      <h1>Welcome To Cheers!!</h1>
+      <p>Come, grab a chair and share your thoughts on some of your favorite (or least favorite) drinks and music! No idea where to start? We got you! Inside, you'll be presented with the oppurtunity to pick
+         your favorite pairs for your drinks/music. The vibes are yours to make!  If your looking for something new, you can visit other peoples pages as well to get some ideas! Pick your drink to enjoy with your song, and vibeee awayy! Have Fun!! 
+      </p>
+
     </main>
   )
-}
+  }
 
 export default Landing
