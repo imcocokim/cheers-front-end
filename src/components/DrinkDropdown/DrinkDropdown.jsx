40cd2db3b2297d288f65275cf9af8057ff680fea
imcoco <imcoco@gmail.com>
2022-07-18 10:51:20 -0400

    refactor MyTastes page with DrinkDropdown component

--- src/components/DrinkDropdown/DrinkDropdown.jsx
+++ src/components/DrinkDropdown/DrinkDropdown.jsx
@@ -1,8 +1,30 @@
-const DrinkDropdown = () => {
+const DrinkDropdown = (props) => {
+  console.log(props)
   return ( 
-    <>
-    </>
+    <select>
+      <option disabled>Wine</option>
+      {props.drinks.map((drink, idx) => (
+        drink.category === "Wine" &&
+        <option key={idx}>
+          {drink.name}
+        </option>
+      ))} 
+      <option disabled>Beer</option>
+      {props.drinks.map((drink, idx) => (
+        drink.category === "Beer" &&
+        <option key={idx}>
+          {drink.name}
+        </option>
+      ))} 
+      <option disabled>Spirit</option>
+      {props.drinks.map((drink, idx) => (
+        drink.category === "Spirit" &&
+        <option key={idx}>
+          {drink.name}
+        </option>
+      ))} 
+  </select>
   );
 }
- 
+
 export default DrinkDropdown;
--- src/pages/MyTastes/MyTastes.jsx
+++ src/pages/MyTastes/MyTastes.jsx
@@ -1,30 +1,9 @@
+import DrinkDropdown from "../../components/DrinkDropdown/DrinkDropdown";
+
 const MyTastes = (props) => {
   return ( 
     <>
-    <select>
-      <option disabled>Wine</option>
-      {props.drinks.map((drink, idx) => (
-        drink.category === "Wine" &&
-        <option key={idx}>
-          {drink.name}
-        </option>
-      ))} 
-      <option disabled>Beer</option>
-      {props.drinks.map((drink, idx) => (
-        drink.category === "Beer" &&
-        <option key={idx}>
-          {drink.name}
-        </option>
-      ))} 
-      <option disabled>Spirit</option>
-      {props.drinks.map((drink, idx) => (
-        drink.category === "Spirit" &&
-        <option key={idx}>
-          {drink.name}
-        </option>
-      ))} 
-
-    </select>
+      <DrinkDropdown drinks={props.drinks}/>
     </>
   )
 }
