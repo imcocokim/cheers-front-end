2145fdb00f854c852dc1031eaf035dee1e937a50
dlobera <delobera@gmail.com>
2022-07-19 13:09:39 -0500

    boozyTune card progress

--- src/components/boozyTuneCard/boozyTuneCard.jsx
+++ src/components/boozyTuneCard/boozyTuneCard.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 const boozyTuneCard = ({}) => {
   return (
-   <div className="card">
-    <img src={
-      
-    } alt="" />
-
-   </div>
+    <div className="card">
+      <img 
+        src={
+        result.strTrackThumb
+      } 
+      alt={`${result.strTrack} album cover`}
+      />
+    </div>
   )
 }
 
