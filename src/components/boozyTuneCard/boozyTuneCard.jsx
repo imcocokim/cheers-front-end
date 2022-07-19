e3dcaf8212f2970522ca73350f05ddfd4260ce3b
dlobera <delobera@gmail.com>
2022-07-19 12:06:00 -0500

    add boozyTunes cards

---
+++ src/components/boozyTuneCard/boozyTuneCard.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const boozyTuneCard = ({}) => {
+  return (
+   <div className="card">
+    <img src={
+      
+    } alt="" />
+
+   </div>
+  )
+}
+
+export default boozyTuneCard;
