00f22511bd6c575510761963b8a2a518311261f7
recipra <jmo0425@outlook.com>
2022-07-18 21:36:07 -0400

    add full api song search functionality

--- src/components/SearchSongForm/SearchSongFrom.jsx
+++ src/components/SearchSongForm/SearchSongFrom.jsx
@@ -1,13 +1,39 @@
-const SearchSongForm = () => {
-  const handleSubmit = event => {
+import { useState } from "react"
+import { searchSongs } from "../../services/songService"
+
+const SearchSongForm = (props) => {
+  const [artist, setArtist] = useState('')
+  const [song, setSong] = useState('')
+
+  const handleSubmit = async (event) => {
     event.preventDefault()
+    const data = await searchSongs(artist, song)
+    props.setResult(data.songs.track[0])
+    console.log(data.songs.track[0])
   }
 
   return (
     <>
       <div>
         <form onSubmit={handleSubmit}>
-          <input type="text" autoComplete="off" />
+          <input
+            type="text"
+            name="artist"
+            autoComplete="off"
+            required
+            placeholder="artist name"
+            value={artist}
+            onChange={(e) => setArtist(e.target.value)}
+          />
+          <input
+            type="text"
+            name="song"
+            autoComplete="off"
+            required
+            placeholder="song name"
+            value={song}
+            onChange={(e) => setSong(e.target.value)}
+          />
           <button type="submit">Search</button>
         </form>
       </div>
---
+++ src/services/songService.js
@@ -0,0 +1,15 @@
+import * as tokenService from '../services/tokenService'
+
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/songs`
+
+async function searchSongs(artist, song) {
+  const path = `${BASE_URL}?artist=${artist}&&song=${song}`
+  const res = await fetch(path, {
+    headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+  })
+  return await res.json()
+}
+
+export {
+  searchSongs
+}
