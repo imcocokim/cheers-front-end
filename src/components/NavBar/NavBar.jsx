521cf57e2aecbf7bcecb0b9785772caaa4563a82
dlobera <delobera@gmail.com>
2022-07-20 11:02:27 -0500

    work on navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,18 +5,19 @@ const NavBar = ({ user, userProfile, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav className={styles.navbar}>
-          {/* <ul className={styles.navitems}> */}
-            <p className={styles.welc}>Welcome, {user.name}</p>
-            <br />
-            <button className={styles.navul}><Link to="/add-song">Add Tune</Link></button>
-            <button className={styles.navul}><Link to="/boozy-tunes">Boozy Tunes</Link></button>
-            <button className={styles.navul}><Link to="/my-tastes">My Tastes</Link></button>
-            <button className={styles.navul}><Link to="/profiles">Profiles</Link></button>
-            <button className={styles.navul}><Link to="/my-page" state={userProfile}>My Page</Link></button>
-            <button className={styles.navul}><Link to="" onClick={handleLogout}>LOG OUT</Link></button>
+        <header className={styles.navbar}>
+          {/* <ul> */}
+          <h1 className={styles.cheers}>CHEERS</h1>
+          <hr class="solid"/>
+            <p className={styles.welc}>Welcome, {user.name}!</p>
+            <li className={styles.navul}><Link to="/add-song">Add Tune</Link></li>
+            <li className={styles.navul}><Link to="/boozy-tunes">Boozy Tunes</Link></li>
+            <li className={styles.navul}><Link to="/my-tastes">My Tastes</Link></li>
+            <li className={styles.navul}><Link to="/profiles">Profiles</Link></li>
+            <li className={styles.navul}><Link to="/my-page" state={userProfile}>My Page</Link></li>
+            <li className={styles.navul}><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
           {/* </ul> */}
-        </nav>
+        </header>
       :
         <nav className={styles.navbar}>
           <ul className={styles.navitems}>
