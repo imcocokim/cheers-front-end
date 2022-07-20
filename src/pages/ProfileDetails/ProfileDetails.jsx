c7eca27e86694cebead228a72746a1df09eff0ec
dlobera <delobera@gmail.com>
2022-07-20 17:35:24 -0500

    css progress

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
 
+
 const ProfileDetails = ({user}) => {
   const location = useLocation()
   const [profile, setProfile] = useState(location.state.profile)
@@ -13,7 +14,7 @@ const ProfileDetails = ({user}) => {
         className="profile-photo"
         style={{width:"300px"}}
       />
-      <h1>{profile.name}</h1> 
+      <h1 className="profcon">{profile.name}</h1> 
       {profile.favoriteSong}
       {profile.favoriteDrink?.name} {profile.favoriteDrink?.category}
       <h2>{profile.name}'s Boozy Tunes</h2>
--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
-
 import Profile from '../../components/Profile/Profile'
+import styles from './Profiles.module.css'
+
 
 const Profiles = ({user}) => {
   const [profiles, setProfiles] = useState([])
@@ -16,13 +17,13 @@ const Profiles = ({user}) => {
 
   return (
     <>
-      <h1>Profiles</h1>
+      <h1 className={styles.profcon}>Profiles</h1>
       {profiles.length ? 
-        <>
+        <div className={styles.profcon}>
           {profiles.map((profile) =>
             <Profile profile={profile} user={user} key={profile._id}/>
           )}
-        </>
+        </div>
       :
         <p>No profiles yet</p>
       }
