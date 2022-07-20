8ad4a9cdc55892cb8473dd7e9dc4182264d34eb2
dlobera <delobera@gmail.com>
2022-07-19 20:12:39 -0500

    add css signup

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -12,7 +12,7 @@ const Signup = props => {
   return (
     <main className={styles.container}>
       <h1>Sign Up</h1>
-      <p>{message}</p>
+      <p className='wel'>{message}</p>
       <SignupForm {...props} updateMessage={updateMessage} />
     </main>
   )
