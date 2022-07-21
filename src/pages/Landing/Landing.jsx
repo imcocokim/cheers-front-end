a601653d5e422144ee1db96ef301aebad95fa0f0
dlobera <delobera@gmail.com>
2022-07-21 14:09:44 -0500

    current css things

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -8,9 +8,6 @@ const Landing = ({ user, handleLogout }) => {
       <p className={styles.par}>Come, grab a chair and share your thoughts on some of your favorite (or least favorite) drinks and music! No idea where to start? We got you! Inside, you'll be presented with the oppurtunity to pick
     Here, mix, match, and discover your favorite pairs of drinks/music. The vibes are yours to make!  If your looking for something new, you can visit other peoples pages as well to get some ideas! Chill, kick ya feet up and vibeee awayy! Have Fun!! 
       </p>
-
-      <h1 className={styles.act}>Activity</h1>
-
     </main>
   )
   }
