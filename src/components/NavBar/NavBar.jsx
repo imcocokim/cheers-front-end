6b63f3a1ec1b62d5c3444e00695597d08f2dac8a
recipra <jmo0425@outlook.com>
2022-07-21 15:03:51 -0400

    remove my tastes link from navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -15,7 +15,6 @@ const NavBar = ({ user, userProfile, handleLogout }) => {
             <p className={styles.welc}>Welcome, {user.name}!</p>
             <li className={styles.navul}><Link to="/add-song">Add Tune</Link></li>
             <li className={styles.navul}><Link to="/boozy-tunes">Boozy Tunes</Link></li>
-            <li className={styles.navul}><Link to="/my-tastes">My Tastes</Link></li>
             <li className={styles.navul}><Link to="/profiles">Profiles</Link></li>
             <li className={styles.navul}><Link to="/my-page" state={userProfile}>My Page</Link></li>
             <li className={styles.navul}><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
