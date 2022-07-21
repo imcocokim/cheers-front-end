4f332a05f89cd758da873ded78d321d81a9ca8f0
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-20 23:33:07 -0400

    work pair for songs and drinks

--- src/pages/BoozyTunesGame/BoozyTunesGame.jsx
+++ src/pages/BoozyTunesGame/BoozyTunesGame.jsx
@@ -1,32 +1,17 @@
 import React, { useEffect, useState } from "react";
-// import * as pollService from "../../services/pollService";
-// import { drinks } from "../../data/drink-data"
-import * as drinkService from "../../services/drinkService"
+import * as boozyTuneGameService from "../../services/boozyTuneGameService"
 
 
 function BoozyTunesGame() {
-  const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}`
-  const [songData, setSongData] = useState([]);
-  const [drinkData, setDrinkData] = useState([]);
-  
+
   useEffect(() => {
-    
-    const fetchDrinkData = async () => {
-      const response = await fetch (`${BASE_URL}/api/drinks`);
-      const newDrinkData = await response.json();
-      setDrinkData(newDrinkData)
+    const fetchRandomDrinkAndSong = async () => {
+      const res = await boozyTuneGameService.getRandomDrinkAndSong()
+      console.log(res)
     };
-    const fetchSongData = async () => {
-      const response = await fetch (`${BASE_URL}/api/songs`);
-      const newSongData =  await response.json();
-      setSongData(newSongData)
-    }
-    fetchDrinkData();
-    fetchSongData();
+    fetchRandomDrinkAndSong();
   }, []);
 
-console.log(songData)
-console.log(drinkData)
 
   return ( 
     <>
@@ -34,49 +19,9 @@ console.log(drinkData)
     <h2> Boozy Tunes Results</h2>
     <h3> 60% of users said YES, THIS SONG goes with THIS DRINK</h3>
     <p>This list of drinks</p>
-    {drinkData?.map(drink => 
-      
-         <p>{drink.name}</p>
-      
-
-      )}
-    {songData?.map(songs => 
-      
-         <p>{songs.name}</p>
-      
-
-      )}
-      
-
-
-
     <button>Continue</button> <br></br><button>Cancel</button>
-
-    <form action= "submit" method="POST">
-    <select name="drink">
-      {/* <input type="radio" id="dewey" name="drone" value="dewey">
-<option value={drink.name}>{drink.name}</option>
-
-
-
-</input> */}
-      {drinkData?.map(drink => 
-<h1>What drink best fits this song</h1>
-
-
-   
-
-   )}
-
-
-      {/* <option value={song.name}>No</option> */}
-
-      
-    </select>
-
-    </form>
     </>
    )
 }
- 
+
   export default BoozyTunesGame;
