6662e8f32a0ed775ea67f3678eb5ae7123420257
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-21 11:03:21 -0400

    check styling

--- src/pages/BoozyTunesGame/BoozyTunesGame.jsx
+++ src/pages/BoozyTunesGame/BoozyTunesGame.jsx
@@ -27,12 +27,8 @@ return (
   <h2> Boozy Tunes Results</h2>
   <h3> 60% of users said YES, THIS SONG goes with THIS DRINK</h3>
   <p>This list of drinks</p>
-    
-       <p>{drinkData?.name}</p>
-    
+       <p>{drinkData?.name}</p>    
        <p>{songData?.name}</p>
-    
-       
        </>
 )
   }
